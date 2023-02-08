cade51c0aece6e5d0d838192fc40df25175c5607
kyle robinson <robinson.k.j@gmail.com>
2023-02-08 13:56:57 +0500

    Updates with dockers

--- Backend/server.js
+++ Backend/server.js
@@ -2,39 +2,75 @@ var express = require("express");
 var url = require("url");
 var mysql = require("mysql");
 var cors = require("cors");
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "sa",
-  password: "saSA@123",
-});
-
-Connect_Database();
+const { fail } = require("assert");
+var sqlite = require("sqlite3").verbose();
 
 const app = express();
 const port = 3000;
-app.use(cors());
+const dbPath = `${__dirname}/db/app_db.db`;
+var db = null;
 
+InitDB();
+
+app.use(cors());
 app.get("/api/countries", (req, res) => {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-
-  con.query(
-    "SELECT Name FROM test_db.countries where Lower(Name) like '%" +
-      query.name +
-      "%'",
-    function (err, result) {
-      if (err) throw err;
-      console.log("Result: " + result);
-      res.send(result);
+  ConnectDB(
+    (db) => {
+      db.all(
+        "SELECT Name FROM Countries where Name like '%" + query.name + "%'",
+        (err, result) => {
+          console.log(err);
+          console.log(result);
+          res.send(result);
+        }
+      );
+    },
+    (err) => {
+      console.log("Connection failed", err);
     }
   );
 });
 
-function Connect_Database() {
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log("Database Server Connected!");
+function ConnectDB(successCallback, failureCallback) {
+  let db = new sqlite.Database(dbPath, sqlite.OPEN_READONLY, (err) => {
+    if (!err) {
+      console.log("Database accessed readonly.");
+      successCallback(db);
+    } else {
+      failureCallback();
+    }
+  });
+}
+
+function InitDB() {
+  db = new sqlite.Database(dbPath, (err) => {
+    if (err) console.log(err);
+    else {
+      let query = "CREATE TABLE IF NOT EXISTS Countries (Name TEXT NOT NULL);";
+      db.run(query, (err) => {
+        if (err) console.log(err);
+        else {
+          let deletequery = "Delete from Countries";
+          db.run(deletequery, () => {
+            if (err) console.log(err);
+            else {
+              let insertquery =
+                "Insert into Countries VALUES ('Pakistan'),('India'),('Maldives'),('Saudi Arab'),('Kuwait'),('Qatar')";
+              db.run(insertquery, (err) => {
+                if (err) console.log(err);
+                else {
+                  db.close((err) => {
+                    if (err) console.log(err);
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
   });
 }
 
